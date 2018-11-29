a387d83b1f2fd686cb343825c2d721fa40bd8f27
Derek <derekjohnbaker23@gmail.com>
2018-11-29 16:02:49 -0600

    1.0

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default () => {
-    return (
-        <div>Comment List</div>
-    );
+class CommentList extends Component {
+    renderComments() {
+        return this.props.comments.map( comment => {
+            return <li>{comment}</li>;
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <ul>
+                    {this.renderComments()}
+                </ul>
+            </div>
+        );
+    }
 }
+
+const mapStateToProps = state => {
+    return { comments: state.comments };
+}
+
+export default connect( mapStateToProps )( CommentList );
